117b3b878a7868f5429cb1060ad43824d0bc05c9
SaiWinOo <saiwinoo52@gmail.com>
2023-12-20 10:36:29 +0630

    add social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { links } from '../../data/links';
+import SocialLinks from './SocialLinks';
 
 
 const Navbar = () => {
@@ -33,26 +34,29 @@ const Navbar = () => {
 
 
   return (
-    <div className='h-auto static lg:sticky top-0 pt-32' >
-      <h1 className='font-bold text-[50px] text-gray-200'>Sai Win Oo</h1>
-      <h4 className='font-semibold text-2xl text-gray-300'>Experienced Fullstack Developer</h4>
-      <p className='text-gray-400'>I build exceptional and accessible digital experiences for the web.</p>
+    <div className='h-[80vh] flex flex-col justify-between  static lg:sticky top-0 pt-32' >
       <div>
-        <nav className="nav hidden lg:block" aria-label="In-page jump links">
-          <ul className="mt-16 w-max">
-            {
-              links.map(link => (
-                <li key={link.href} >
-                  <a className={`group flex items-center py-3 ${link.href === activeLink ? 'nav-active' : ''}`} href={link.href}>
-                    <span className='nav-line mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
-                    <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">{link.text}</span>
-                  </a>
-                </li>
-              ))
-            }
-          </ul>
-        </nav>
+        <h1 className='font-bold text-[50px] text-gray-200'>Sai Win Oo</h1>
+        <h4 className='font-semibold text-2xl text-gray-300'>Experienced Fullstack Developer</h4>
+        <p className='text-gray-400 my-5'>I build exceptional and accessible digital experiences for the web.</p>
+        <div>
+          <nav className="nav hidden lg:block" aria-label="In-page jump links">
+            <ul className="mt-16 w-max">
+              {
+                links.map(link => (
+                  <li key={link.href} >
+                    <a className={`group flex items-center py-3 ${link.href === activeLink ? 'nav-active' : ''}`} href={link.href}>
+                      <span className='nav-line mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                      <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">{link.text}</span>
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+        </div>
       </div>
+      <SocialLinks />
     </div >
   )
 }
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FiTwitter } from "react-icons/fi";
+import { PiLinkedinLogoBold } from "react-icons/pi";
+
+const links = [
+  { github: 'https://www.github.com/saiwinoo13' },
+  { instagram: 'https://www.facebook.com/saiwinoo13' },
+  { twitter: 'https://www.facebook.com/saiwinoo13' },
+  { linkedin: 'https://www.facebook.com/saiwinoo13' },
+];
+
+const SocialLinks = () => {
+  return (
+    <ul className='flex gap-4'>
+      <li>
+        <a href={links.github} target='_blank' className='hover:text-white text-gray-400 cursor-pointer'>
+          <FaGithub className='text-2xl hover:scale-110 transition-all duration-300' />
+        </a>
+      </li>
+      <li>
+        <a href={links.instagram} className='hover:text-white text-gray-400 cursor-pointer'>
+          <FaInstagram className='text-2xl hover:scale-110 transition-all duration-300' />
+        </a>
+      </li>
+      <li>
+        <a href={links.twitter} className='hover:text-white text-gray-400 cursor-pointer'>
+          <FiTwitter className='text-2xl hover:scale-110 transition-all duration-300' />
+        </a>
+      </li>
+      <li>
+        <a href={links.linkedin} className='hover:text-white text-gray-400 cursor-pointer'>
+          <PiLinkedinLogoBold className='text-2xl hover:scale-110 transition-all duration-300' />
+        </a>
+      </li>
+
+
+    </ul>
+  )
+}
+
+export default SocialLinks;
