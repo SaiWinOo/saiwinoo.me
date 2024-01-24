389b9dfb75a2dbaca55b1a2893b3291e3e3adc32
SaiWinOo <saiwinoo52@gmail.com>
2024-01-24 19:04:10 +0630

    add meta tag

---
+++ src/app/head.js
@@ -0,0 +1,45 @@
+export default function Head() {
+  return (
+    <>
+      <title>
+        Sai Win Oo - Software Engineer
+      </title>
+      <meta
+        content="width=device-width, initial-scale=1"
+        name="viewport"
+      />
+      <meta
+        name="description"
+        content="Sai Win Oo - Software Engineer And Tech Enthusiast"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/me.jpeg"
+      />
+      {/* Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://saiwinoo.tech/" />
+      <meta
+        property="og:title"
+        content="Sai Win Oo - Software Engineer And Tech Enthusiast"
+      />
+      <meta
+        property="og:description"
+        content="He is a Software Engineer who is passionate about developing web application."
+      />
+
+      {/*    Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://saiwinoo.tech/" />
+      <meta
+        property="twitter:title"
+        content="Sai Win Oo - Software Engineer And Tech Enthusiast"
+      />
+      <meta
+        property="twitter:description"
+        content="He is a Software Engineer who is passionate about developing web application."
+      />
+    </>
+  )
+}
