4b0ad9a27f360e4c42a8b5a46afb6e2e4f742e3e
SaiWinOo <saiwinoo52@gmail.com>
2024-01-26 22:59:13 +0630

    update for seo

--- src/app/head.js
+++ src/app/head.js
@@ -12,34 +12,28 @@ export default function Head() {
         name="description"
         content="Sai Win Oo - Software Engineer And Tech Enthusiast"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        href="/me.jpeg"
-      />
-      {/* Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://saiwinoo.tech/" />
-      <meta
-        property="og:title"
-        content="Sai Win Oo - Software Engineer And Tech Enthusiast"
-      />
-      <meta
-        property="og:description"
-        content="He is a Software Engineer who is passionate about developing web application."
-      />
+      <link rel="icon" href="/favicon.ico" />
 
-      {/*    Twitter */}
+      {/* Primary */}
+      <title>Sai Win Oo - Software Engineer And Tech Enthusiast</title>
+      <meta name="title" content="Sai Win Oo - Software Engineer And Tech Enthusiast" />
+      <meta name="description" content="He is a Software Engineer who is passionate about developing web application." />
+
+      {/* Facebook  */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Sai Win Oo - Software Engineer And Tech Enthusiast" />
+      <meta property="og:description" content="He is a Software Engineer who is passionate about developing web application." />
+      <meta property="og:image" content="https://i.postimg.cc/CLgG5C8X/myself.jpg" />
+
+      {/* Twitter  */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://saiwinoo.tech/" />
-      <meta
-        property="twitter:title"
-        content="Sai Win Oo - Software Engineer And Tech Enthusiast"
-      />
-      <meta
-        property="twitter:description"
-        content="He is a Software Engineer who is passionate about developing web application."
-      />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Sai Win Oo - Software Engineer And Tech Enthusiast" />
+      <meta property="twitter:description" content="He is a Software Engineer who is passionate about developing web application." />
+      <meta property="twitter:image" content="https://i.postimg.cc/CLgG5C8X/myself.jpg" />
+
+
     </>
   )
 }
